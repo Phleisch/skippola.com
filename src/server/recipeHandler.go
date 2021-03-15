b6f68f7c15db1975453d8988ee73e11f33e86212
Kai Fleischman <kaifleischmans@gmail.com>
2021-03-14 23:26:16 -0500

    add recipe structs

--- src/server/recipeHandler.go
+++ src/server/recipeHandler.go
@@ -1,13 +1,37 @@
 package recipesHandler
 
 import (
+	"json"
+	"ioutil"
 	"strings"
 	"net/http"
 )
 
+// struct for a recipe; all fields are required and both ingredients and
+// instructions lists must have at least one item each
+type recipe struct {
+	Name			string			`json:"name"`
+	PrepTime		string			`json:"prepTime"`
+	TotalTime		string			`json:"totalTime"`
+	Ingredients		[]ingredient	`json:"ingredients"`
+	Instructions	[]string		`json:"instructions"`
+}
+
+// struct for an ingredient in a recipe; name is required, amount and prep are
+// optional
+type ingredient struct {
+	Name	string	`json:"name"`
+	Amount	string	`json:"amount"`
+	Prep	string	`json:"prep"`
+}
+
+func generateRecipePage(r recipe, pageTemplate, pathName string) {
+
+}
+
 // Handle all requests for specific recipe pages for any URL beginning with the
 // path "/recipes/"
-func recipesPageHandler(write http.ResponseWriter, request *http.Request) {
+func PageHandler(write http.ResponseWriter, request *http.Request) {
 	// get the recipe page to fetch from the URL
 	recipeName := strings.TrimPrefix(request.URL.Path, "/recipes/")
 
@@ -16,8 +40,8 @@ func recipesPageHandler(write http.ResponseWriter, request *http.Request) {
 	if err != nil { fmt.Println(err) }
 
 	// unpack the json file into golang struct representation of the recipe
-	var recipe Recipe
-	err = json.Unmarshal(recipeJson, &recipe)
+	var r recipe
+	err = json.Unmarshal(recipeJson, &r)
 	if err != nil { fmt.Println(err) }
 
 	// open the recipe page template for formatting
@@ -26,10 +50,10 @@ func recipesPageHandler(write http.ResponseWriter, request *http.Request) {
 
 	// format the recipe page template with specifics of the recipe to generate
 	// the recipe's webpage
-	generateRecipePage(recipe, string(recipeWebpageTemplate), recipeName)
+	generateRecipePage(r, string(recipeWebpageTemplate), recipeName)
 }
 
 // Handle all requests for the recipe index page at "/recipes"
-func recipesIndexHandler(writer http.ResponseWriter, request *http.Request) {
+func IndexHandler(writer http.ResponseWriter, request *http.Request) {
 
 }
